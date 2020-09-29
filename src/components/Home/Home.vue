7838698b42fe46121b17b7807b299a6b884a6319
Matteo Ena <matteo.ena@netcentric.biz>
2020-09-29 13:50:41 +0200

    Initial conf

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div id="house"></div>
+    <div id="garden"></div>
+    <div id="sun"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import 'Home.scss';
+</style>
